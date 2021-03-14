0b2ec6c399ad4406de7834ce237a204396291e82
ShaunaChow <z1023778132@icloud.com>
2021-03-14 20:08:18 +0800

    '重新部署'

--- common_server/src/main/java/top/shauna/shaunaiscoming2/common/dao/impl/ShaunaDfsDaoImpl.java
+++ common_server/src/main/java/top/shauna/shaunaiscoming2/common/dao/impl/ShaunaDfsDaoImpl.java
@@ -61,7 +61,7 @@ public class ShaunaDfsDaoImpl implements ShaunaDfsDao {
         pubConfig.setThreadPoolNums(10);
         pubConfig.setTimeout(100000L);
         if (pubConfig.getRegisterBean() == null) {
-            RegisterBean registerBean = new RegisterBean("zookeeper", "39.105.89.185:2181", null);
+            RegisterBean registerBean = new RegisterBean("zookeeper", "8.131.57.153:2181", null);
             pubConfig.setRegisterBean(registerBean);
         }
         if (pubConfig.getFoundBean() == null) {
